6100beab7ec9a6b6da6e4d4e8e991cb437288f4a
Jovan , Popovski <Jovan.PopOvski@pearson.com>
2020-01-17 19:51:24 +0100

    Added Cucumber options

--- src/test/java/resources/TestRunner.java
+++ src/test/java/resources/TestRunner.java
@@ -6,7 +6,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
+@CucumberOptions(features={"resources.features.SauceLabs"},glue = "stepDefinitions",
+        tags = {"@test"})
 public class TestRunner {
 
 }
